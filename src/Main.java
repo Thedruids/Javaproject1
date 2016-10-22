4a71be0f22769f90a633c3fb019675ca5637dc69
Thedruids <The_only_0ne@hotmail.com>
2016-10-22 14:40:41 +0100

    2nd Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-/**
- * Created by Thomas on 17/10/2016.
- */
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        System.out.println("This is a new commit to the Google Cloud SDK");
 
     }
 }
